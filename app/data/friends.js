1985cb382ef54614200604d1458caa0e16e0e140
kidffiti <coreyash10@gmail.com>
2019-06-11 13:14:05 -0400

    css updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var allFriends = [
     {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Homer",
+    "photo":"https://www.kcbi.org/wp-content/uploads/2019/03/homer-simpson.jpg",
     "scores":[
         5,
         1,
@@ -14,7 +14,52 @@ var allFriends = [
         4,
         1
       ]
-  }
+  },
+  {
+    "name":"Dwayne 'The Rock' Johnson",
+    "photo":"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDU1MjM5ODg2MzUx/dwayne-johnson-11818916-1-402.jpg",
+    "scores": [ 4, 1, 2, 4, 3, 5, 3, 4, 3, 1 ]
+  },
+  {
+    "name":"Ash Ketchum",
+    "photo":"https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/P630464_01",
+    "scores": [ 2, 1, 5, 2, 2, 1, 2, 5, 5, 2 ]
+  },
+  {
+    "name":"Ryan Reynolds",
+    "photo":"https://data.junkee.com/wp-content/uploads/2016/02/deadpool-movie-2015-thumbs-up-mditd0fn2czy3ps40jx65quje2tgdkslj5zcgyqd4o.jpg",
+    "scores": [ 2, 4, 3, 3, 5, 2, 2, 5, 5, 5 ]
+  },
+  {
+    "name":"Sandra Bullock",
+    "photo":"https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/12/17/07/sandra-bullock.jpg",
+    "scores": [ 5, 4, 1, 1, 2, 4, 2, 2, 2, 4 ]
+  },
+  {
+    "name":"Scarlett Johansson",
+    "photo":"https://media.vanityfair.com/photos/58dab0903753ee611fd24790/master/w_768,c_limit/Scarlett-Johansson.jpg",
+    "scores": [ 2, 2, 3, 1, 1, 3, 4, 3, 5, 3 ]
+  },
+  {
+    "name":"Linus Torvalds",
+    "photo":"https://www.bbc.com/news/technology-45664640",
+    "scores": [ 3, 3, 5, 2, 2, 3, 4, 1, 5, 3 ]
+  },
+  {
+    "name":"Donald Trump",
+    "photo":"https://cdn.theatlantic.com/assets/media/img/mt/2016/09/RTX1GZCO/lead_720_405.jpg?mod=1533691850",
+    "scores": [ 3, 3, 4, 2, 1, 2, 4, 4, 2, 5 ]
+  },
+  {
+    "name":"Barack Obama",
+    "photo":"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2017%2F11%2Fbarack-obama1.jpg%3Fw%3D2000&w=450&c=sc&poi=face&q=85",
+    "scores": [ 5, 4, 1, 1, 2, 4, 2, 2, 2, 4 ]
+  },
+  {
+    "name":"Fred",
+    "photo":"https://vignette.wikia.nocookie.net/youtube/images/c/cd/FRED.jpg/revision/latest?cb=20160204123432",
+    "scores": [ 1, 4, 4, 3, 3, 1, 5, 5, 1, 1 ]
+  },
 ]
 
 module.exports = allFriends
